561ade18783af0dbd5b02d719afa5ceb39bce3d4
Basilews <dethroll@gmail.com>
2018-09-11 14:27:15 +0300

    add new styles & tooltip

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import persons from '../assets/data.json'
+import 'normalize.css';
 import './style.css';
 
 
@@ -45,12 +46,14 @@ class App extends Component {
     const { query, persons } = this.state;
 
     return (
-      <div className="content">
+      <div className="personList">
         <h3 className="title">My team for this test</h3>
-        <label>
-          Add team member to this test:
-          <select value="" onChange={event => this.handleChange(event.target)}>
-            <option value=''>add team member</option>
+        <div className="persons">
+          <select
+            value=""
+            className="selectBox"
+            onChange={event => this.handleChange(event.target)}>
+            <option value=''>Add team member</option>
             {persons.map(person =>
               <option key={person.id} value={person.id}>{person.username}</option>
             )}
@@ -58,13 +61,27 @@ class App extends Component {
           {query && query.length > 0 && (
             <ul>
               {query.map(person => (
-                <li key={`person-${person.id}`} onClick={() => this.deletePerson(person)}>
-                  {person.username}
+                <li
+                  key={`person-${person.id}`}
+                  className="person">
+                  <div className="personPicture">
+                    <img
+                      width="32"
+                      height="32"
+                      className="image"
+                      src={`assets/${person.picture}`}/>
+                    <div className="close" onClick={() => this.deletePerson(person)} />
+                    <div className="tooltip">Remove user</div>
+                  </div>
+                  <div>
+                    <div>{person.role}</div>
+                    <div><b>{person.username}</b></div>
+                  </div>
                 </li>
               ))}
             </ul>
           )}
-        </label>
+        </div>
       </div>
     );
   }
